8c6f894494566425f3716b071d85680143bd41f9
Marco Pugliese <126952052+Marco-Pugliese@users.noreply.github.com>
2023-10-03 16:57:01 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const hiddenBtn = Array.from(document.getElementsByClassName("nascondi"));
+const saldi = document.getElementsByClassName("saldi-content")[0];
+const welcomeSummer = document.getElementsByClassName(
+  "welcome-summer-content"
+)[0];
+
+hiddenBtn[0].addEventListener("click", () => {
+  saldi.classList.toggle("d-none");
+  if (saldi.classList.contains("d-none")) {
+    hiddenBtn[0].innerText = " Mostra sezione";
+  } else {
+    hiddenBtn[0].innerText = "Nascondi sezione";
+  }
+});
+hiddenBtn[1].addEventListener("click", () => {
+  welcomeSummer.classList.toggle("d-none");
+  if (welcomeSummer.classList.contains("d-none")) {
+    hiddenBtn[1].innerText = " Mostra sezione";
+  } else {
+    hiddenBtn[1].innerText = "Nascondi sezione";
+  }
+});
+
+const allTravels = Array.from(document.getElementsByClassName("card-body"));
+alert(`Attenzione, ci sono ${allTravels.length} viaggi disponibili`);
+
+const destroyThePage = () => {
+  const allImg = Array.from(document.getElementsByTagName("img"));
+  const allCards = Array.from(document.getElementsByClassName("card"));
+  for (let i = 0; i < allImg.length; i++) {
+    allImg[i].classList.add("d-none");
+  }
+  for (let y = 0; y < allCards.length; y++) {
+    allCards[y].classList.add("d-none");
+  }
+};
+destroyThePage();
